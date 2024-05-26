b296c4091485a8ff549bea1e80d33816a4fcb2e5
OliPohl <oliver@pohl66.de>
2024-05-26 06:24:14 +0200

    loaded config into photographer class

--- rtspPhotographer.py
+++ rtspPhotographer.py
@@ -1,6 +1,7 @@
 import json
 import os
 import threading
+from threading import Event
 import time
 from watchdog import observers
 from watchdog.events import FileSystemEventHandler
@@ -8,15 +9,16 @@ from watchdog.events import FileSystemEventHandler
 class ConfigLoader:
     def __init__(self, file_path):
         self.file_path = file_path
+        self.on_config_load = Event()
         
         if not os.path.exists(self.file_path):
             print(f"\nConfiguration file not found at {self.file_path}")
-            self.create_config()
-        self.streams = self.load_config()
-        self.setup_watchdog()
+            self._create_config()
+        self.streams = self._load_config()
+        self._setup_watchdog()
 
 
-    def create_config(self):
+    def _create_config(self):
         data = {
             "streams": [
                 {
@@ -31,41 +33,48 @@ class ConfigLoader:
             print(">>> Please add your RTSP streams to the configuration file. <<<")
 
 
-    def load_config(self):
+    def _load_config(self):
         with open(self.file_path, 'r') as file:
             data = json.load(file)
         
         print("\nLoaded configuration file:")
         for stream in data['streams']:
             print(f"## Stream: {stream['name']}, URL: {stream['url']}")
+            
+        self.on_config_load.set()
         return data['streams']
 
 
-    def setup_watchdog(self):
+    def get_config(self):
+        return self.streams
+
+
+    def _setup_watchdog(self):
         event_handler = FileSystemEventHandler()
-        event_handler.on_modified = self.watchdog_on_modified
+        event_handler.on_modified = self._watchdog_on_modified
         self.observer = observers.Observer()
         self.observer.schedule(event_handler, path=os.path.dirname(self.file_path), recursive=False)
         self.debounce_time = 2
         self.debounce_timer = None
         
-        def start_observer():
+        def __start_observer():
             print(f"\nWatchdog initialized")
             self.observer.start()
             while self.observer.is_alive():
                 self.observer.join(1)
         
-        self.observer_thread = threading.Thread(target=start_observer)
+        self.observer_thread = threading.Thread(target=__start_observer)
         self.observer_thread.start()
 
 
-    def watchdog_on_modified(self, event):
+    def _watchdog_on_modified(self, event):
         if (event.src_path == self.file_path):
             if self.debounce_timer is not None:
                 self.debounce_timer.cancel()
             
-            self.debounce_timer = threading.Timer(self.debounce_time, self.load_config)
+            self.debounce_timer = threading.Timer(self.debounce_time, self._load_config)
             self.debounce_timer.start()
+
 
 
     def interrupt(self):
@@ -76,20 +85,53 @@ class ConfigLoader:
 
 
 class Photographer:
-    def __init__(self):
+    def __init__(self, output_dir, config_loader):
+        self.output_dir = output_dir
+        self.config_loader = config_loader
+        
+        self._load_streams()
+        
+        self.config_event_thread = threading.Thread(target=self._wait_for_config_load_event)
+        self.config_event_thread.start()
+
+
+    def _wait_for_config_load_event(self):
+        self.config_loader.on_config_load.wait()
+
+        self._load_streams()
+        self.config_event_thread.join()
+        self.config_event_thread.start()
+        
+        
+    def interrupt(self):
+        self.config_event_thread.join()
+        print("Config Event terminated")
+
+
+    def _load_streams(self):
+
         pass
 
 
 def main():
-    config_loader = ConfigLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json"))
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    
+    config_loader = ConfigLoader(os.path.join(current_dir, "config.json"))
+    photographer = Photographer(current_dir, config_loader)
     
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         config_loader.interrupt()
+        photographer.interrupt()
         print("rtspPhotographer is no longer running")
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# TODO: Put progromm in autstart?
