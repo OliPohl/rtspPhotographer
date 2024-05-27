437949bd8a43af6dae013f776f6751a5f24048a6
OliPohl <oliver@pohl66.de>
2024-05-27 07:10:17 +0200

    updated winrun

--- winrun.bat
+++ winrun.bat
@@ -1,3 +1,26 @@
 @echo off
-call ".venv/Scripts/activate.bat"
+
+REM Check if python is installed
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python is not installed.
+    pause
+)
+
+REM Check if pip is installed
+where pip >nul 2>nul
+if %errorlevel% neq 0 (
+    echo pip is not installed.
+    pause
+)
+
+REM Check if the .venv directory exists
+if not exist ".venv" (
+    python -m venv .venv
+    call ".venv\Scripts\activate.bat"
+    pip install -r requirements.txt
+) else (
+    call .venv\Scripts\activate
+)
+
 python -m rtspPhotographer
