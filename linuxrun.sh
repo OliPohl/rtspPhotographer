b97e31aee29849fd108358726761fa0a5426aec8
OliPohl <oliver@pohl66.de>
2024-05-30 13:00:37 +0200

    apt instead of apt-get

--- linuxrun.sh
+++ linuxrun.sh
@@ -5,13 +5,13 @@ cd "$(dirname "$0")"
 dpkg-query -W -f='${Status}' python3-venv 2>/dev/null | grep -q "ok installed"
 if [ $? -ne 0 ]; then
     echo "venv module not found. Installing..."
-    sudo apt-get install python3-venv
+    sudo apt install python3-venv
 fi
 
 pip3 --version &> /dev/null
 if [ $? -ne 0 ]; then
     echo "pip not found. Installing..."
-    sudo apt-get install python3-pip
+    sudo apt install python3-pip
 fi
 
 if [ ! -d ".venv" ]; then
